1a62fc3026c2d2b76f19eb6b7c945fbaee10ed06
Ron Fernandez <ronbfernandez@gmail.com>
2021-03-20 23:06:46 -0700

    Updating file on WeatherPy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "332b6b163d64de36664e5f20910963fa"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyCCPUIParfj5jFKvRfzbyN4rO9-55mX16o"
+g_key = "YOUR KEY HERE"
  
